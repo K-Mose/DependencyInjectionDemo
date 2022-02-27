70f59e66591e543867bf94997c4b07ad94a43bd8
user <km_413@naver.com>
2022-02-27 16:18:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dependencyinjectiondemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.dependencyinjectiondemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val smartPhone = SmartPhone(
+            Battery(),
+            SIMCard(ServiceProvider()),
+            MemoryCard()
+        )
+            .makeACallWithRecording()
+    }
+}
